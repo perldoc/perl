3bde3b3009bf96c9125ba379cd6cdc1df965783f
Perl Doc <yonghei7777@gmail.com>
2018-04-25 07:54:05 +0900

    post method add

--- http_sender/sender.pl
+++ http_sender/sender.pl
@@ -3,49 +3,51 @@ use strict;
 use warnings;
 use LWP::UserAgent;
 use 5.010;
-use LWP::UserAgent;
 
 my %target = &target;
 my %request = &request;
 
 for my $url (keys %target) {
+	my $ua = LWP::UserAgent->new;
+	my $server_endpoint = "http://" . $url . ":$target{$url}" . $request{"url"};
+	
 
-	if($target{$url} > 0){
-		
-		my $ua = LWP::UserAgent->new;
-		my $server_endpoint = "http://" . $url . ":$target{$url}" . $request{"url"};
-
-		# set custom HTTP request header fields
+	
+	# set custom HTTP request header fields
+	if($request{"method"} eq "GET"){
 		my $req = HTTP::Request->new(GET => $server_endpoint);
-		for (keys %request) {
-			next if /method|url/;
-			$req->header($_ => $request{$_});
-		}
-		
+	} elsif($request{"method"} eq "POST") {
+		my %post_data = map { split /=/ } split /&/, $request{"POST_DATA"};
+		my $req = HTTP::Request->new(POST => $server_endpoint, [ %post_data ]); # [ %post_data ] 이해 필요
 		my $resp = $ua->request($req);
 		if ($resp->is_success) {
 			print "HTTP GET response code: ", $resp->code, "\n";
 			print "HTTP GET response message: ", $resp->message, "\n";
-			
+
 			my $header = $resp->headers_as_string;
 			print "Received Header : $header\n";
-		
+
 			my $message = $resp->decoded_content;
 			print "Received reply: $message\n";
-		}
-		else {
+		} else {
 			print "HTTP GET error code: ", $resp->code, "\n";
 			print "HTTP GET error message: ", $resp->message, "\n";
-			
+
 			my $header = $resp->headers_as_string;
-			print "Received Header : $header\n";
-		
+			#print "Received Header : $header\n";
+
 			my $message = $resp->decoded_content;
-			print "Received reply: $message\n";
-
-
+			#print "Received reply: $message\n";
 		}	
 	}
+
+#	#header manual setup
+#	for (keys %request) {
+#		next if /method|url|POST_DATA/;
+#		$req->header($_ => $request{$_});
+#	}
+
+
 }
 
 sub target {
@@ -69,6 +71,7 @@ sub target {
 	close(FH2);
 	return %target_port;
 }
+
 sub request {
 	my $request = 'request.txt';
 	open FH1, "<$request" || die "Cannot open $request.\n";
@@ -82,12 +85,26 @@ sub request {
 	$request{"url"} = $url;
 	
 	foreach (<FH1>){
-	  my ($title, $value) = split /:/;
-	  $title =~ s/^\s+|\s+$//g;
-	  $value =~ s/^\s+|\s+$//g;
-	  $request{$title} = $value;
+	if(/.+=.+&*/){
+		$request{"POST_DATA"} = $_ ;
+		next;
 	}
-
+	next if /^\s*$/;
+	
+	my ($title, $value) = split /:/;
+	$title =~ s/^\s+|\s+$//g;
+	$value =~ s/^\s+|\s+$//g;
+	$request{$title} = $value;
+	}
+	
+#	my %test = map { split /=/ } split /&/, $request{"POST_DATA"};
+#
+#	foreach (keys %test) {
+#		say "$_ : $test{$_}";
+#	}	
+	foreach (keys %request) {
+		say "$_ : $request{$_}";
+	}
 	close(FH1);
 	return %request;
 }
